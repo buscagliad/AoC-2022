e69a9e04ffcb0eea1baaccc3b4d1e1e9e438ac34
David Buscaglia <david.buscaglia@vts-i.com>
2023-01-24 17:04:17 -0800

    day 16 - need a good solution

--- day16/valves-new.cc
+++ day16/valves-new.cc
@@ -84,6 +84,63 @@ typedef struct valve_s {
 } valve;
 
 vector<valve> volcano;
+int	shortestPath[50][50];
+
+
+// shortestPath[i][j] is the minimum number of minutes required to go
+//     from volcano[i] to volcano[j]
+// OBVIOUSLY shortestPath[k][k] is zero
+//           shortestPath[k][m] = shortestPath[m][k]
+
+void out(vector<valve> &v, int shortestPath[50][50])
+{
+	for (int i = 0; i < (int) v.size(); i++) printf(",%s", v[i].nm.c_str());
+	printf("\n");
+	for (int i = 0; i < (int) v.size(); i++)
+	{
+		printf("%s", v[i].nm.c_str());
+		for (int j = 0; j < (int) v[j].tunix.size(); j++)
+		{
+			printf(",%d", shortestPath[i][j]);
+		}
+		printf("\n");
+	}
+}
+	
+void initShortPaths(vector<valve> &v, int shortestPath[50][50])
+{
+	for (int i = 0; i < (int) v.size(); i++)
+	{
+		for (int k = 0; k < (int) v[k].tunix.size(); k++)
+		{
+			shortestPath[i][k] = 1;
+			shortestPath[k][i] = 1;
+		}
+	}
+}
+
+void getShortPaths(vector<valve> &v, int shortestPath[50][50], int pathcnt = 1)
+{
+	for (int i = 0; i < (int) v.size(); i++)
+	{
+		for (int j = i + 1; j < (int) v.size(); j++)
+		{
+			// look for each connector nearby, if the path is shorter
+			// that the current path, select the path
+			for (int k = 0; k < (int) v[j].tunix.size(); k++)
+			{
+				//int	c = v[i].tunix[k];
+				if (shortestPath[i][k] == 0) shortestPath[i][k] = pathcnt;
+				if (v[i].tunix[k] == i)
+				{
+					shortestPath[i][k] = pathcnt + 1;
+					shortestPath[k][i] = pathcnt + 1;
+				}
+			}
+		}
+	}
+}
+
 
 bool bigger (int a, int b) { return a > b; }
 
@@ -281,7 +338,6 @@ void		next_move(vector<valve> &v, path_state ps)
 	
 
 }
-		
 
 
 void getchars(char *inp, char *nm, int num)
@@ -335,6 +391,7 @@ void init(const char *fn)
 		{
 			int tunix = nm_index(volcano, volcano[i].tunnels[j]);
 			volcano[i].tunix.push_back(tunix);
+			shortestPath[i][j] = 0;
 		}
 	}
 	fclose(f);
@@ -403,54 +460,20 @@ void solvept2(const char *fn, int answer)
 {
 	init(fn);
 	output(volcano);
-	vector<valve> v = volcano;
-	//output(nv);
-	path_state ps;
-	ps.minute = 0;
-	ps.pressure = 0;
-	ps.new_pressure = 0;
-	ps.total = 0;
-	ps.path = volcano[0].nm;
-	ps.valves_left_to_open = numGTzero(volcano);
-	setflows(volcano, ps);
-	int aa = nm_index(volcano, "AA");
-		maxps = ps;
-	
-	if (0)
-	{
-    MinutesToRun = 26;
-		ps.minute = 9;
-		ps.pressure = 78;
-		ps.new_pressure = 0;
-		ps.total = 414-78;
-		ps.openvalves = "DD JJ BB HH CC";
-		ps.valves_left_to_open = 1;
-		ps.prevvalve1 = nm_index(volcano, "CC");
-		ps.atvalve1 = nm_index(volcano, "CC");
-		ps.prevvalve2 = nm_index(volcano, "FF");
-		ps.atvalve2 = nm_index(volcano, "EE");
-		ps.flows_remaining[0] = 3;
-		next_move(v, ps);
-		printf("\n\ngPath\n");
-		outps(maxps);
-		printf("Flow: %d\n", maxps.pressure);
-		exit(1);
-	}		
 
-	outps(ps);
     MinutesToRun = 26;
-    maxps = ps;
+	int aa = nm_index(volcano, "AA");
 
 	for (size_t k = 0; k < volcano[aa].tunix.size() - 1; k++)
 	{
 		for (size_t m = k+1; m < volcano[aa].tunix.size(); m++)
 		{
-			path_state nps = ps;
+			path_state nps;
 			nps.prevvalve1 = aa;
-			nps.atvalve1 = v[aa].tunix[k];
+			nps.atvalve1 = volcano[aa].tunix[k];
 			nps.prevvalve2 = aa;
-			nps.atvalve2 = v[aa].tunix[m];
-			next_move(v, nps);
+			nps.atvalve2 = volcano[aa].tunix[m];
+			next_move(volcano, nps);
 		}
 	}
 	printf("\n\ngPath\n");
@@ -463,7 +486,7 @@ void solvept2(const char *fn, int answer)
 		printf("Answer is not correct - should be %d\n", answer);
 }
 
-int main()
+int main5()
 {
 
 	//solvept1("ex.txt", 1651);
@@ -471,6 +494,138 @@ int main()
 	//solvept1("input.txt", 1906);  
 	solvept2("ex.txt", 1707);
 	solvept2("input.txt", 2548);
+	return 0;
+}
+
+
+// CPP code for printing shortest path between
+// two vertices of unweighted graph
+#include <bits/stdc++.h>
+using namespace std;
+
+// utility function to form edge between two vertices
+// source and dest
+void add_edge(vector<int> adj[], int src, int dest)
+{
+	adj[src].push_back(dest);
+	adj[dest].push_back(src);
+}
+
+// a modified version of BFS that stores predecessor
+// of each vertex in array p
+// and its distance from source in array d
+bool BFS(vector<int> adj[], int src, int dest, int v,
+		int pred[], int dist[])
+{
+	// a queue to maintain queue of vertices whose
+	// adjacency list is to be scanned as per normal
+	// DFS algorithm
+	list<int> queue;
+
+	// boolean array visited[] which stores the
+	// information whether ith vertex is reached
+	// at least once in the Breadth first search
+	bool visited[v];
+
+	// initially all vertices are unvisited
+	// so v[i] for all i is false
+	// and as no path is yet constructed
+	// dist[i] for all i set to infinity
+	for (int i = 0; i < v; i++) {
+		visited[i] = false;
+		dist[i] = INT_MAX;
+		pred[i] = -1;
+	}
+
+	// now source is first to be visited and
+	// distance from source to itself should be 0
+	visited[src] = true;
+	dist[src] = 0;
+	queue.push_back(src);
+
+	// standard BFS algorithm
+	while (!queue.empty()) {
+		int u = queue.front();
+		queue.pop_front();
+		for (int i = 0; i < (int) adj[u].size(); i++) {
+			if (visited[adj[u][i]] == false) {
+				visited[adj[u][i]] = true;
+				dist[adj[u][i]] = dist[u] + 1;
+				pred[adj[u][i]] = u;
+				queue.push_back(adj[u][i]);
+
+				// We stop BFS when we find
+				// destination.
+				if (adj[u][i] == dest)
+					return true;
+			}
+		}
+	}
+
+	return false;
+}
+
+// utility function to print the shortest distance
+// between source vertex and destination vertex
+void printShortestDistance(vector<int> adj[], int s,
+						int dest, int v)
+{
+	// predecessor[i] array stores predecessor of
+	// i and distance array stores distance of i
+	// from s
+	int pred[v], dist[v];
+
+	if (BFS(adj, s, dest, v, pred, dist) == false) {
+		cout << "Given source and destination"
+			<< " are not connected";
+		return;
+	}
+
+	// vector path stores the shortest path
+	vector<int> path;
+	int crawl = dest;
+	path.push_back(crawl);
+	while (pred[crawl] != -1) {
+		path.push_back(pred[crawl]);
+		crawl = pred[crawl];
+	}
+
+	// distance from source is in distance array
+	cout << "Shortest path length is : "
+		<< dist[dest];
+
+	// printing path from source to destination
+	cout << "\nPath is::\n";
+	for (int i = path.size() - 1; i >= 0; i--)
+		cout << path[i] << " ";
+}
+
+// Driver program to test above functions
+int main()
+{
+	// no. of vertices
+	int v = 8;
+
+	// array of vectors is used to store the graph
+	// in the form of an adjacency list
+	vector<int> adj[v];
+
+	// Creating graph given in the above diagram.
+	// add_edge function takes adjacency list, source
+	// and destination vertex as argument and forms
+	// an edge between them.
+	add_edge(adj, 0, 1);
+	add_edge(adj, 0, 3);
+	add_edge(adj, 1, 2);
+	add_edge(adj, 3, 4);
+	add_edge(adj, 3, 7);
+	add_edge(adj, 4, 5);
+	add_edge(adj, 4, 6);
+	add_edge(adj, 4, 7);
+	add_edge(adj, 5, 6);
+	add_edge(adj, 6, 7);
+	int source = 0, dest = 7;
+	printShortestDistance(adj, source, dest, v);
 	return 0;
 }
 
